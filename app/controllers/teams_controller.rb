8c5a96c143b7cba77369b8ef0cda4890e1b5ad15
Ritesh <railsriteshc56@gmail.com>
2020-04-23 17:29:34 +0530

    Added Team model

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,13 @@
+class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
+
+  # GET /teams
+  # GET /teams.json
+  def index
+    @teams = Team.all
+  end
+
+  def raw_data
+    @teams = Team.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :teams
+  get 'raw_data', to: 'teams#raw_data'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
