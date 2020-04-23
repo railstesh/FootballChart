dfa771f1a01e2d401f2b28fe4968fad0278bfcbf
Ritesh <railsriteshc56@gmail.com>
2020-04-23 18:04:13 +0530

    Added Team page and raw data page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,9 +1,21 @@
 class TeamsController < ApplicationController
+
   def index
     @teams = Team.all
+    @min_diff_team = get_min_diff("goal_hit", "goal_against")
   end
 
   def raw_data
-    @teams = Team.all
+    @teams = File.open("public/football.dat").read
+  end
+
+  private
+
+  def get_min_diff(goal_hit, goal_against)
+    @teams.sort_by{|team| get_diff(team[goal_hit].to_i, team[goal_against].to_i).abs}.first
+  end
+
+  def get_diff(goal_hit, goal_against)
+    (goal_hit - goal_against).abs
   end
 end
